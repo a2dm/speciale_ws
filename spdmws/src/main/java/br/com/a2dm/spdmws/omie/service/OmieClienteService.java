765d5385c09a84b9cc5e0f55b9e60ed21ac56cdb
Mateus <mateusbcavalcante@gmail.com>
2021-03-26 11:57:37 -0300

    Obtendo codigos da tabela de preco e cliente

--- spdmws/src/main/java/br/com/a2dm/spdmws/omie/service/OmieClienteService.java
+++ spdmws/src/main/java/br/com/a2dm/spdmws/omie/service/OmieClienteService.java
@@ -1,8 +1,15 @@
 package br.com.a2dm.spdmws.omie.service;
 
+import java.math.BigInteger;
+import java.util.Date;
 import java.util.List;
 
+import br.com.a2dm.spdm.entity.Cliente;
+import br.com.a2dm.spdm.service.ClienteService;
 import br.com.a2dm.spdmws.dto.ClienteDTO;
+import br.com.a2dm.spdmws.omie.payload.ClienteWebhookPayload;
+import br.com.a2dm.spdmws.omie.payload.TabelaPrecoPayload;
+import br.com.a2dm.spdmws.omie.payload.TagPayload;
 import br.com.a2dm.spdmws.omie.repository.OmieClientesRepository;
 
 public class OmieClienteService {
@@ -18,12 +25,80 @@ public class OmieClienteService {
 		}
 		return instance;
 	}
-	
+
 	public List<ClienteDTO> pesquisarClientes(String nomeCliente) throws OmieServiceException {
 		try {
 			return OmieClientesRepository.getInstance().pesquisarClientes(nomeCliente);
 		} catch (Exception e) {
 			throw new OmieServiceException(e);
 		}
+	}
+
+	public void processar(ClienteWebhookPayload clienteWebhookPayload) throws OmieServiceException {
+		try {
+			
+			String nomeTabelaPreco = obterTabelaPreco(clienteWebhookPayload.getEvent().getTags());
+			TabelaPrecoPayload tabelaPreco = OmieTabelaPrecoService.getInstance().obterTabelaPreco(nomeTabelaPreco);
+			
+			Cliente cliente = new Cliente();
+			cliente.setIdExternoOmie(clienteWebhookPayload.getEvent().getCodigo_cliente_omie());
+
+			cliente = ClienteService.getInstancia().get(cliente, 0);
+
+			if (cliente == null) {
+				cliente = new Cliente();
+				cliente.setDesCliente(clienteWebhookPayload.getEvent().getRazao_social());
+
+				cliente = ClienteService.getInstancia().get(cliente, 0);
+
+				if (cliente == null) {
+					inserirCliente(clienteWebhookPayload, tabelaPreco);
+				} else {
+					alterarCliente(clienteWebhookPayload, cliente, tabelaPreco);
+				}
+			} else {
+				alterarCliente(clienteWebhookPayload, cliente, tabelaPreco);
+			}
+		} catch (Exception e) {
+			throw new OmieServiceException(e);
+		}
+	}
+	
+	private String obterTabelaPreco(List<TagPayload> tags) {
+		for (TagPayload tag: tags) {
+			if (isTagTabelaPreco(tag)) {
+				return tag.getTag();
+			}
+		}
+		return null;
+	}
+
+	private void inserirCliente(ClienteWebhookPayload clienteWebhookPayload, TabelaPrecoPayload tabelaPreco) throws Exception {
+		Cliente clienteInsert = new Cliente();
+		clienteInsert.setDesCliente(clienteWebhookPayload.getEvent().getRazao_social());
+		clienteInsert.setIdExternoOmie(clienteWebhookPayload.getEvent().getCodigo_cliente_omie());
+		clienteInsert.setIdTabelaPrecoOmie(tabelaPreco.getnCodTabPreco());
+		clienteInsert.setFlgAtivo("S");
+		clienteInsert.setDatCadastro(new Date());
+		clienteInsert.setIdUsuarioCad(new BigInteger("1"));
+		ClienteService.getInstancia().inserir(clienteInsert);
+	}
+
+	private void alterarCliente(ClienteWebhookPayload clienteWebhookPayload, Cliente cliente, TabelaPrecoPayload tabelaPreco) throws Exception {
+		cliente.setDesCliente(clienteWebhookPayload.getEvent().getRazao_social());
+		cliente.setIdExternoOmie(clienteWebhookPayload.getEvent().getCodigo_cliente_omie());
+		cliente.setIdTabelaPrecoOmie(tabelaPreco.getnCodTabPreco());
+		if (isTopicExcluido(clienteWebhookPayload)) {
+			cliente.setFlgAtivo("N");
+		}
+		ClienteService.getInstancia().alterar(cliente);
+	}
+	
+	private boolean isTagTabelaPreco(TagPayload tag) {
+		return tag.getTag().indexOf("TP") == 0;
+	}
+
+	private boolean isTopicExcluido(ClienteWebhookPayload clienteWebhookPayload) {
+		return clienteWebhookPayload.getTopic().equalsIgnoreCase("ClienteFornecedor.Excluido");
 	}
 }
--- spdmws/src/main/java/br/com/a2dm/spdmws/ws/ClientesWS.java
+++ spdmws/src/main/java/br/com/a2dm/spdmws/ws/ClientesWS.java
@@ -2,7 +2,9 @@ package br.com.a2dm.spdmws.ws;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -11,6 +13,7 @@ import javax.ws.rs.core.MediaType;
 import br.com.a2dm.spdmws.dto.ClienteDTO;
 import br.com.a2dm.spdmws.exception.ApiException;
 import br.com.a2dm.spdmws.exception.ExceptionUtils;
+import br.com.a2dm.spdmws.omie.payload.ClienteWebhookPayload;
 import br.com.a2dm.spdmws.omie.service.OmieClienteService;
 
 @Path("/clientes")
@@ -22,6 +25,17 @@ public class ClientesWS {
 	public List<ClienteDTO> pesquisarClientes(@PathParam("nomeCliente") String nomeCliente) throws ApiException {
 		try {
 			return OmieClienteService.getInstance().pesquisarClientes(nomeCliente);
+		} catch (Exception e) {
+			throw ExceptionUtils.handlerApiException(e);
+		}
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void processar(ClienteWebhookPayload clienteWebhookPayload) throws ApiException {
+		try {
+			OmieClienteService.getInstance().processar(clienteWebhookPayload);
 		} catch (Exception e) {
 			throw ExceptionUtils.handlerApiException(e);
 		}
